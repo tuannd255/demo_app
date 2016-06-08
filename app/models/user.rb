42d21fd624a60c809235f521a74dff4e7cec5f94
tuannd255 <tuannd255@gmail.com>
2016-06-08 16:21:58 +0700

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ActiveRecord::Base
   	enum gender: ['other', 'male', 'female', 'less', 'gay']
   	validates :birthday, presence: true
   	validate :check_birthday
+    # Returns the hash digest of the given string.
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
   	private
 
