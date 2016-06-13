590a7ef24de13531691635b0bf547c331a9a1c4f
tuannd255 <tuannd255@gmail.com>
2016-06-13 16:12:00 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :entries, only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
