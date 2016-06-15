1c2454411abf583ee8e7912e1c5edc27c956ef0f
tuannd255 <tuannd255@gmail.com>
2016-06-15 13:49:30 +0700

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	def show
 	    @user = User.find(params[:id])
 	    @entries = @user.entries.paginate(page: params[:page])
+	    @entry  = current_user.entries.build
 	end
 	
 	def new
